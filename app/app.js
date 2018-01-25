ca3ed2b70c181012b7c486d846c782c52e343ad1
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-25 13:50:49 -0500

    added login functionality

--- app/app.js
+++ app/app.js
@@ -9,6 +9,12 @@ class App {
     App.renderBoardOptions()
     App.chooseBoard.addEventListener("change", App.handleBoardChoice)
     App.colors = ["#FBBC05","EA4335","#34A853","#4285F4"]
+    App.loginButton = document.getElementById("login")
+    App.createAccountButton = document.getElementById("create-account")
+    App.initiateUser = document.getElementById("initiate-user")
+    App.loginButton.addEventListener("click", App.renderLoginForm)
+    App.createAccountButton.addEventListener("click", App.renderCreateAccountForm)
+    App.userInfo = document.getElementById("user-info")
   }
 
   static async handleBoardChoice(event){
@@ -56,6 +62,115 @@ class App {
     // if ((event.target.tagName.toLowerCase() === 'span') && (event.type === 'mouseout')){
     //   event.target.style.background = 'white'
     // }
+  }
+
+  static renderLoginForm(event){
+    event.preventDefault();
+    let loginForm = document.createElement("form");
+    loginForm.id = "login-form"
+
+    let usernameLabel = document.createElement("label")
+    usernameLabel.htmlFor = "username"
+    usernameLabel.innerText = "Username:"
+
+    App.username = document.createElement("input");
+    App.username.type = "text";
+    App.username.id = "username";
+
+    let submit = document.createElement("input")
+    submit.type = "submit"
+    submit.value = "Login"
+    loginForm.append(usernameLabel)
+    loginForm.append(App.username)
+    loginForm.append(submit)
+
+    App.navBar.removeChild(App.initiateUser)
+    App.navBar.append(loginForm)
+
+    // App.userInfo.append(loginForm)
+    loginForm.addEventListener("submit", App.fetchUser)
+  }
+
+  static renderCreateAccountForm(event){
+    event.preventDefault();
+    let createAccountForm = document.createElement("form");
+    createAccountForm.id = "create-account-form"
+
+    let usernameLabel = document.createElement("label")
+    usernameLabel.htmlFor = "username"
+    usernameLabel.innerText = "Username:"
+
+    App.username = document.createElement("input");
+    App.username.type = "text";
+    App.username.id = "username";
+
+    let submit = document.createElement("input")
+    submit.type = "submit"
+    submit.value = "Create Account"
+    createAccountForm.append(usernameLabel)
+    createAccountForm.append(App.username)
+    createAccountForm.append(submit)
+
+    App.navBar.removeChild(App.initiateUser)
+    App.navBar.append(createAccountForm)
+
+    createAccountForm.addEventListener("submit", App.createUser)
+  }
+
+  static async createUser(event){
+    event.preventDefault()
+    if (App.username.value.match(/\W/)){
+      alert("Username cannot contain non-letter characters.")
+    } else {
+      let userJSON = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+        body: JSON.stringify({name: App.username.value})
+      }).then(r => r.json())
+      if (userJSON["errors"]){
+        alert(userJSON["errors"][0])
+      } else {
+        App.user = new User(userJSON)
+        App.renderUserInfo()
+      }
+    }
+  }
+
+  static async fetchUser(event){
+    event.preventDefault()
+    if (App.username.value.match(/\W/)){
+      alert("Username cannot contain non-letter characters.")
+    } else {
+      let userJSON = await fetch(`http://localhost:3000/api/v1/users/${App.username.value}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+      }).then(r => r.json())
+      if (userJSON["errors"]){
+        alert(userJSON["errors"][0])
+      } else {
+        App.user = new User(userJSON)
+        App.renderUserInfo()
+      }
+    }
+  }
+
+  static renderUserInfo(){
+    App.navBar.removeChild(App.navBar.lastChild)
+    App.welcomeMessage = document.createElement("p")
+    App.welcomeMessage.innerText = `Welcome, ${App.user.name}!  `
+
+    let logout = document.createElement("button")
+    logout.id = "logout"
+    logout.innerText= "Logout"
+
+    App.welcomeMessage.append(logout)
+    App.navBar.append(App.welcomeMessage)
   }
 
 
