521b2831e9f9f2d28f407954b19cc7a27a1515e9
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-28 14:40:43 -0500

    polished maneuverability with keypress handler on board

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ class App {
     App.board = document.querySelector(".board")
     App.hints = document.getElementById("hints")
     App.info = document.getElementById("info")
-    App.colors = ["#FBBC05","EA4335","#34A853","#4285F4"]
+    App.colors = ["rgba(251, 188, 5, 0.4)","rgba(234, 67, 53, 0.4)","rgba(52, 168, 83, 0.4)","rgba(66, 133, 244, 0.4)"]
     App.renderLoginInfo()
     App.welcomeJSON =
     {"acrossmap":null,"admin":false,"answers":{"across":["WELCOME","CROSSWORD"],"down":["CLOOGLE","TO", "BINDING", "CRY"]},"author":"Sebastian Royer","autowrap":null,"bbars":null,"circles":null,"clues":{"across":["4. Be well and enter","5. The name of the game"], "down":["1. Powered by...","2. 'Across 4. __ Down 1!''","3. When I say ___ ...","5. ... you say ___!"]},"code":null,"copyright":"2018, Cloogle","date":"26\/91\/2018","dow":"Friday","downmap":null,"editor":"Matt McAlister","grid":['.','.','C','.','T','.','.','.','B','W','E','L','C','O','M','E','.','I','.','.','O','.','.','.','.','.','N','C','R','O','S','S','W','O','R','D','R','.','G','.','.','.','.','.','I','Y','.','L','.','.','.','.','.','N','.','.','E','.','.','.','.','.','G'],"gridnums":[0,0,1,0,2,0,0,0,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"hold":null,"id":null,"id2":null,"interpretcolors":null,"jnotes":null,"key":null,"mini":null,"notepad":null,"publisher":"Cloogle Adventures","rbars":null,"shadecircles":null,"size":{"cols":9,"rows":7},"title":"WELCOME CROSSWORD","track":null,"type":null}
@@ -40,74 +40,86 @@ class App {
     App.board.innerHTML = ''
     App.board.addEventListener('mouseover', App.boardHoverHandler)
     App.board.addEventListener('mouseout', App.boardHoverHandler)
+    App.board.addEventListener("click", App.setCurrentTile)
     App.board.addEventListener("focusin", App.boardFocusHandler)
     App.board.addEventListener("focusout", App.boardFocusHandler)
-    App.board.addEventListener('keyup', App.handleKeyInput)
     App.board.addEventListener('keydown', App.handleKeyDown)
+    App.board.addEventListener('keypress', App.handleKeyPress)
     App.hints.innerHTML = "<dl id='across'> <dt>Across</dt> </dl> <dl id='down'> <dt>Down</dt> </dl>"
     App.hints.addEventListener('mouseover', App.hintHoverHandler)
     App.hints.addEventListener('mouseout', App.hintHoverHandler)
     App.info.innerHTML = ''
   }
 
-  static hintHoverHandler(event) {
-  	if ((event.target.tagName.toLowerCase() === 'dd') && (event.type === 'mouseover')){
-      if (event.target.parentNode.id === 'down'){
-        event.target.style.background = App.colors[2]
-        document.querySelectorAll(`[data-down='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
-      }
-      if (event.target.parentNode.id === 'across'){
-        event.target.style.background = App.colors[0]
-        document.querySelectorAll(`[data-across='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
-      }
+  static setCurrentTile(event){
+    if (event.target.tagName === "INPUT"){
+      App.currentTile = event.target
     }
-    if ((event.target.tagName.toLowerCase() === 'dd') && (event.type === 'mouseout')){
-      event.target.style.background = ''
-      if (event.target.parentNode.id === 'down'){
-        document.querySelectorAll(`[data-down='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = '' })
+  }
+
+  static hintHoverHandler(event) {
+    if (!App.currentTile) {
+    	if ((event.target.tagName.toLowerCase() === 'dd') && (event.type === 'mouseover')){
+        if (event.target.parentNode.id === 'down'){
+          event.target.style.background = App.colors[2]
+          document.querySelectorAll(`[data-down='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
+        }
+        if (event.target.parentNode.id === 'across'){
+          event.target.style.background = App.colors[0]
+          document.querySelectorAll(`[data-across='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
+        }
       }
-      if (event.target.parentNode.id === 'across'){
-        document.querySelectorAll(`[data-across='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = '' })
+      if ((event.target.tagName.toLowerCase() === 'dd') && (event.type === 'mouseout')){
+        event.target.style.background = ''
+        if (event.target.parentNode.id === 'down'){
+          document.querySelectorAll(`[data-down='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = '' })
+        }
+        if (event.target.parentNode.id === 'across'){
+          document.querySelectorAll(`[data-across='${event.target.id.split('-')[1]}']`).forEach(function(cell){ cell.style.background = '' })
+        }
       }
     }
   }
 
 
   static boardHoverHandler(event) {
-    if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'mouseover')){
+    if (!App.currentTile) {
+      if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'mouseover')){
 
-      if (event.target.dataset.across){
-        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
-        document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
-        document.querySelectorAll(`#across-${event.target.dataset.across}`).forEach(function(dd){dd.style.background = App.colors[0]})
-      }
-      if (event.target.dataset.down) {
-        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
-        document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
-        document.querySelectorAll(`#down-${event.target.dataset.down}`).forEach(function(dd){dd.style.background = App.colors[2]})
-      }
-      event.target.style.background = App.colors[3]
+        if (event.target.dataset.across){
+          document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
+          document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
+          document.querySelectorAll(`#across-${event.target.dataset.across}`).forEach(function(dd){dd.style.background = App.colors[0]})
+        }
+        if (event.target.dataset.down) {
+          document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
+          document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
+          document.querySelectorAll(`#down-${event.target.dataset.down}`).forEach(function(dd){dd.style.background = App.colors[2]})
+        }
+        event.target.style.background = App.colors[3]
 
-    }
-    if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'mouseout')){
-      event.target.style.background = ''
-      if (event.target.dataset.across){
-        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = '' })
-        document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = '' })
-        document.querySelectorAll(`#across-${event.target.dataset.across}`).forEach(function(dd){dd.style.background = ''})
       }
-      if (event.target.dataset.down) {
-        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = '' })
-        document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = '' })
-        document.querySelectorAll(`#down-${event.target.dataset.down}`).forEach(function(dd){dd.style.background = ''})
+      if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'mouseout')){
+        event.target.style.background = ''
+        if (event.target.dataset.across){
+          document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = '' })
+          document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = '' })
+          document.querySelectorAll(`#across-${event.target.dataset.across}`).forEach(function(dd){dd.style.background = ''})
+        }
+        if (event.target.dataset.down) {
+          document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = '' })
+          document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = '' })
+          document.querySelectorAll(`#down-${event.target.dataset.down}`).forEach(function(dd){dd.style.background = ''})
+        }
       }
     }
   }
 
   static boardFocusHandler(event) {
     if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'focusin')){
+      App.currentTile = event.target
       if (event.target.dataset.across){
-        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0] })
+        document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = App.colors[0]  })
         document.querySelectorAll(`[data-down='${event.target.dataset.down}']`).forEach(function(cell){ cell.style.background = App.colors[2] })
         document.querySelectorAll(`#across-${event.target.dataset.across}`).forEach(function(dd){dd.style.background = App.colors[0]})
       }
@@ -120,6 +132,7 @@ class App {
 
     }
     if ((event.target.tagName.toLowerCase() === 'input') && (event.type === 'focusout')){
+      App.currentTile = undefined
       event.target.style.background = ''
       if (event.target.dataset.across){
         document.querySelectorAll(`[data-across='${event.target.dataset.across}']`).forEach(function(cell){ cell.style.background = '' })
@@ -328,14 +341,10 @@ class App {
 
   static checkCompletion(){
     if (!Array.from(App.board.querySelectorAll("input")).some(el => el.value === "")){
-      // console.log("complete")
       if (App.checkValid()){
-        // valid board
         App.onSuccess()
       } else {
-        // not valid board
         App.onFail()
-                // console.log("not valid")
       }
     }
 
@@ -398,22 +407,50 @@ class App {
     }
   }
 
-  static handleKeyInput(event){
-    App.checkCompletion()
-    if (App.user) {
+
+  static handleKeyPress(event){
+    if (App.user && event.key.length === 1) {
       App.updateUserProgress(event)
+    }
+    App.setDirection(event)
+
+      if (App.direction === "across") {
+        App.focusOnNextInputInRow(event)
+      } else {
+        App.focusOnNextInputInColumn(event)
+      }
+    App.checkCompletion()
+  }
+
+  static traverseCurrentWord(event){
+    if (event.key === "Backspace"){
+      (App.direction === "across") ? App.focusOnPreviousInputInRow(event) : App.focusOnPreviousInputInColumn(event)
+    }
+  }
+
+  static setDirection(event){
+    if (event.shiftKey) {
+      App.direction = "down"
+    } else {
+      App.direction = "across"
     }
   }
 
   static handleKeyDown(event){
+    App.setDirection(event)
+    App.traverseCurrentWord(event)
     if (event.key === "ArrowLeft"){
       App.focusOnPreviousInputInRow(event)
+      App.setDirection(event)
     } else if (event.key === "ArrowRight"){
       App.focusOnNextInputInRow(event)
+      App.setDirection(event)
     } else if (event.key === "ArrowUp"){
       App.focusOnPreviousInputInColumn(event)
+      App.setDirection(event)
     } else if (event.key === "ArrowDown"){
       App.focusOnNextInputInColumn(event)
+      App.setDirection(event)
     }
   }
 
@@ -429,6 +466,7 @@ class App {
       nextElement = document.getElementById(`item${row}-${newColumn}`)
     }
     nextElement.focus()
+    App.currentTile = nextElement
   }
 
   static focusOnPreviousInputInRow(event){
@@ -443,6 +481,7 @@ class App {
       previousElement = document.getElementById(`item${row}-${newColumn}`)
     }
     previousElement.focus()
+    App.currentTile = previousElement
   }
 
   static focusOnNextInputInColumn(event){
@@ -457,6 +496,7 @@ class App {
       nextElement = document.getElementById(`item${newRow}-${column}`)
     }
     nextElement.focus()
+    App.currentTile = nextElement
   }
 
   static focusOnPreviousInputInColumn(event){
@@ -471,6 +511,7 @@ class App {
       previousElement = document.getElementById(`item${newRow}-${column}`)
     }
     previousElement.focus()
+    App.currentTile = previousElement
   }
 
   static async updateUserProgress(event){
@@ -480,7 +521,7 @@ class App {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
         },
-      body: JSON.stringify({"user_id": App.user.id, "board_id": App.currentBoard.id, "index": event.target.dataset.index, "value": event.target.value})
+      body: JSON.stringify({"user_id": App.user.id, "board_id": App.currentBoard.id, "index": event.target.dataset.index, "value": event.key})
     }).then(r => r.json())
     if (response['errors']){
       console.log(response['errors'][0])
